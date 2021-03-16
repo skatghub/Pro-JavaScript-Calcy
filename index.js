35aa10db208830f0d953b67d86e4a61e03e23653
skatghub <80279260+skatghub@users.noreply.github.com>
2021-03-16 15:07:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
 let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        
